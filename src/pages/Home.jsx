daae2bff679e8b2297968ff9db21685419b113d8
Kevin Jerome <blade94781kj@gmail.com>
2025-08-06 19:47:24 -0400

    Added background image and other features to website

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,7 @@ export default function Home() {
   return (
     <div className="home-container">
       <div className="home-header">
-        <h1 className="app-title">OctaneNexus</h1>
+        <h1 className="app-title">Welcome To OctaneNexus</h1>
 
         <div className="nav-controls">
           <input
